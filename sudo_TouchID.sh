9ca9bdc91e67e79ce984fd12bab640c4f662cc3a
Marcin Mitura <mitura07@wp.pl>
2023-09-16 14:50:29 +0200

    Prevent Apple nuking changes with macOS updates
    I'm a dumbass who just discovered, you can actually do things like that wooow.
    
    Enjoy xD

--- sudo_TouchID.sh
+++ sudo_TouchID.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
 patchsudo() {
-  if grep -q 'pam_tid.so' '/etc/pam.d/sudo'; then
-    echo "Touch ID authentication is already present in /etc/pam.d/sudo"
+  CUSTOM_SUDO_PAM='/etc/pam.d/sudo_custom'
+  DEFAULT_SUDO_PAM='/etc/pam.d/sudo'
+
+  # Check if the custom PAM configuration file exists
+  if [ ! -e "$CUSTOM_SUDO_PAM" ]; then
+    echo "Creating custom PAM configuration for sudo..."
+    
+    # Copy the default sudo PAM configuration to the custom file
+    sudo cp "$DEFAULT_SUDO_PAM" "$CUSTOM_SUDO_PAM"
+    
+    # Add Touch ID authentication to the custom PAM configuration
+    sudo sh -c 'echo "auth       sufficient     pam_tid.so" >> "$CUSTOM_SUDO_PAM"'
+    
+    echo "Touch ID authentication added to sudo."
   else
-    echo "Adding Touch ID authentication ('auth sufficient pam_tid.so') to 
-/etc/pam.d/sudo..."
-    sudo sed -i '' '1s/^/auth sufficient pam_tid.so\n/' '/etc/pam.d/sudo'
-    echo "Touch ID authentication applied successfully"
+    echo "Custom PAM configuration for sudo already exists."
   fi
 }
 
 patchsudo
+
+echo "Script execution completed."
