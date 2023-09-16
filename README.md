67029b1d268fa097da3d9d7b1725aead3aa75946
Marcin Mitura <mitura07@wp.pl>
2023-09-16 17:22:50 +0200

    Serious things in not serious update.
    Testing my edgy nerd writting if anyone cares, lol.
    
    From serious shit,
    currently script checks if it's Sonoma or older and applies respectful changes:
    
    a) uses `sudo_local.template` from Apple
    
    or
    
    b) uses my custom PAM config file.
    
    - Added an ability to test out if it works after.

--- README.md
+++ README.md
@@ -1,19 +1,27 @@
 # sudo with Touch ID Authentication
 
-A simple Bash Script to enhance your macOS experience by enabling Touch ID as a sudo command verification method. 
+This is the cringest README I've ever done, but
 
-### Requirements:
+Inject some rawr-worthy energy into your macOS world with this edgy Bash Script that empowers your inner hacker by unleashing Touch ID as a sudo command verification method. 
 
-- A Mac with Touch ID support (because why not make your Mac even cooler?)
+**RawrXD Note**: This script is your rebel companion, perfectly harmonizing with macOS 14 (Sonoma) and beyond. 🤘 <br>
+But if you're still dwelling in the past with macOS Ventura (13) or older, brace yourself for potential script re-runs after updates.<br> 
+Stay wild! 🖤
 
-- An active brain, but don't worry; we won't ask you any tricky questions.
+### Dark Requirements:
 
-### Usage:
+- A Mac with Touch ID support (because your Mac needs to be as edgy as you are!)
+
+- An active, brooding brain, but don't sweat it; we won't be throwing any mainstream questions your way.
+
+- macOS Sonoma, so you can join the underground elite. 🌑
+
+### How to Join the Dark Side:
 
 1. [Download the script](sudo_TouchID.sh).
 
-2. Double-click to run it.
+2. Double-click like a true rebel.
 
-3. Brace yourself for the magic of Touch ID authentication!
+3. Get ready for the mind-blowing Touch ID authentication experience! Rawr xD 🦇💀
 
-**Please be aware that your super-secret password is still required for security reasons. Safety first!**
+**Warning**: Your super-secret password is still needed for security reasons. We may be rebels, but we're responsible rebels. Stay safe! 🔒
--- sudo_TouchID.sh
+++ sudo_TouchID.sh
@@ -1,25 +1,45 @@
-#!/bin/sh
+#!/bin/bash
 
-patchsudo() {
-  CUSTOM_SUDO_PAM='/etc/pam.d/sudo_custom'
-  DEFAULT_SUDO_PAM='/etc/pam.d/sudo'
+update_sudo_pam() {
+  # Define the paths for sudo PAM configuration files
+  SUDO_PAM="/etc/pam.d/sudo"
+  CUSTOM_SUDO_PAM="/etc/pam.d/sudo_custom"
 
-  # Check if the custom PAM configuration file exists
-  if [ ! -e "$CUSTOM_SUDO_PAM" ]; then
-    echo "Creating custom PAM configuration for sudo..."
+  # Check if the macOS version is Sonoma (macOS 14) or newer
+  if [[ "$(sw_vers -productVersion)" > "14."* ]]; then
+    # Check if the sudo_local file exists
+    if [[ ! -e "$SUDO_LOCAL" ]]; then
+      # Copy the template file to sudo_local
+      sudo cp "$SUDO_LOCAL_TEMPLATE" "$SUDO_LOCAL"
+      echo "Created sudo_local configuration file."
+    fi
+
+    # Uncomment the line to enable Touch ID in sudo_local
+    sudo sed -i '' '/^# auth       sufficient     pam_tid.so/s/^# //' "$SUDO_LOCAL"
     
-    # Copy the default sudo PAM configuration to the custom file
-    sudo cp "$DEFAULT_SUDO_PAM" "$CUSTOM_SUDO_PAM"
-    
-    # Add Touch ID authentication to the custom PAM configuration
-    sudo sh -c 'echo "auth       sufficient     pam_tid.so" >> "$CUSTOM_SUDO_PAM"'
-    
-    echo "Touch ID authentication added to sudo."
+    echo "Touch ID authentication is now set up for sudo."
   else
-    echo "Custom PAM configuration for sudo already exists."
+    # For Ventura (macOS 13) and older, use the custom PAM method
+    if [[ ! -e "$CUSTOM_SUDO_PAM" ]]; then
+      echo "Creating custom PAM configuration for sudo..."
+      sudo cp "$SUDO_PAM" "$CUSTOM_SUDO_PAM"
+      sudo sh -c 'echo "auth       sufficient     pam_tid.so" >> "$CUSTOM_SUDO_PAM"'
+      echo "Custom PAM configuration for sudo created."
+    else
+      echo "Custom PAM configuration for sudo already exists."
+    fi
+  fi
+
+  # Prompt to test Touch ID authentication
+  read -p "Do you want to test Touch ID authentication for sudo? (y/n): " test_choice
+  if [[ "$test_choice" == "y" || "$test_choice" == "Y" ]]; then
+    sudo -v
+    if [[ $? -eq 0 ]]; then
+      echo "Touch ID authentication for sudo is working correctly!"
+    else
+      echo "Touch ID authentication for sudo failed. Please check your configuration."
+    fi
   fi
 }
 
-patchsudo
-
-echo "Script execution completed."
+update_sudo_pam
